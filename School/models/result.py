b0afa0c9ebc7d703e141dd32c61fb3f46551c63f
Manan-Painter <mananpainter056@gmail.com>
2024-04-15 16:16:00 +0530

    [some work]

--- School/models/result.py
+++ School/models/result.py
@@ -15,7 +15,7 @@ class exam_attendance(models.Model):
 
 
 class BaseClass(models.Model):
-    _name = "base.model"
+    _name = " "
     _description = "Base Class"
 
     name = fields.Char("Base Name")
