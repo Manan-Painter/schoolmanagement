efa229a93801091248f44b67bcfbe1e97b41e63d
Manan-Painter <mananpainter056@gmail.com>
2024-04-30 15:21:17 +0530

    [context]

--- school/wizard/admission_cancle.py
+++ school/wizard/admission_cancle.py
@@ -9,3 +9,9 @@ class AdmissionCancel(models.Model):
     reason = fields.Char(string="Reason")
     wiz_admission_id = fields.Many2one("admission.student", "Admission")
 
+
+    def action_admission_cancel(self):
+        drive_obj = self.env['admission.student'].browse(self._context.get("active_id"))
+        drive_obj.cancel_reason = self.reason
+        drive_obj.status = "cancelled"
+
